7f0a50fd09ce74ae9cdf9610c7c418bd53122ff4
Pieter Allen <pieter.allen@binus.ac.id>
2025-04-21 23:19:15 +0700

    #feat authContext & test bot

---
+++ components/context/AuthContext.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { Session, User } from "@supabase/supabase-js";
+import { useRouter } from "next/router";
+import { supabase } from "@/supabase";
+
+interface AuthContextType {
+    user : User | null;
+    session : Session | null;
+    loading : boolean;
+    signOut : () => Promise<void>;
+}
+
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({children} : {children : React.ReactNode}) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [session, setSession] = useState<Session | null>(null);
+    const [loading, isLoading] = useState<boolean>(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const getSession = async () => {
+            const {data, error} = await supabase.auth.getSession();
+            if (data.session){
+                setUser(data.session.user);
+                setSession(data.session);
+            }
+            isLoading(false);
+        }
+
+        getSession();
+
+        const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+            setUser(session?.user || null);
+          });
+      
+          return () => {
+            listener.subscription.unsubscribe();
+          };
+
+    }, [])
+
+
+    const signOut = async () =>{
+        await supabase.auth.signOut();
+        setUser(null);
+        setSession(null);
+    }   
+
+    return (
+        <AuthContext.Provider value={{user, session, loading, signOut}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("useAuth must be used within an AuthProvider");
+    return context;
+};
+  
+
