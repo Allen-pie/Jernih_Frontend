bc4d9d8de02d2848e49fe3cd1fa3029c5ef9041e
KevinLiong <kevin.liong@binus.ac.id>
2025-05-03 08:45:14 +0700

    # feat create articles page

---
+++ app/articles/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DashboardHeader } from "@/components/dashboard-header"
+
+const Article = () => {
+    return (
+        <div className='flex min-h-screen flex-col'>
+            <DashboardHeader />
+            Articles
+        </div>
+    );
+}
+
+export default Article;
