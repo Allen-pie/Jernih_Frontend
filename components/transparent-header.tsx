abd123fb7ad220288f5e81e87398f100b2dd84cb
Pieter Allen <pieter.allen@binus.ac.id>
2025-05-27 16:38:28 +0700

    #fix transparant header component for login & register

---
+++ components/transparent-header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+export function TransparentHeader(){
+    return (
+        <header className="absolute z-99 top-4">
+            <Image
+                      src={'/assets/jernihLogo.svg'}
+                      width={150}
+                      height={100}
+                      alt="Jernih Logo"
+                      className="transition-opacity duration-300 opacity-100"
+            />
+        </header>
+    )
+}
