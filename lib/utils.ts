30e0b00c113eaf7c1249da1f71e4734e51a51852
Pieter Allen <pieter.allen@binus.ac.id>
2025-05-27 16:34:40 +0700

    #fix move fetch img url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,44 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { supabase } from '@/supabase';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function fetchImageUrl(path: string): Promise<string> {
-  try {
-    const { data: publicUrlData, error: publicUrlError } = supabase
-      .storage
-      .from('jernih') // Your bucket name
-      .getPublicUrl(`${path}`);
-
-    if (publicUrlError) {
-      console.error('Error fetching public URL:', publicUrlError);
-      return '';
-    }
-
-    return publicUrlData?.publicUrl || '';
-  } catch (error: any) {
-    console.error('Error fetching image:', error.message);
-    return '';
-  }
-}
-
-export async function fetchDefaultImageUrl(): Promise<string> {
-  try {
-    const { data: publicUrlData, error: publicUrlError } = supabase
-      .storage
-      .from('jernih') // Bucket name
-      .getPublicUrl('locations/default-image.jpg'); // Path to default image
-
-    if (publicUrlError) {
-      console.error('Error fetching default image URL:', publicUrlError);
-      return '';
-    }
-
-    return publicUrlData?.publicUrl || '';
-  } catch (error: any) {
-    console.error('Error fetching default image:', error.message);
-    return '';
-  }
-}
