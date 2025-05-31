255576d3ba6a97f1b4a2f3c0596342abbf7917ef
Pieter Allen <pieter.allen@binus.ac.id>
2025-05-31 11:55:56 +0700

    #fix change name again

---
+++ hooks/use-image.ts
@@ -0,0 +1,26 @@
+// hooks/useImage.ts
+import { useEffect, useState } from "react";
+import { supabase } from '@/utils/supabase/client'
+
+export function useImage(path: string) {
+  const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const { data, error } = await supabase
+        .from("assets")
+        .select("path")
+        .is("model_id", null)
+        .eq("path", path);
+
+      if (error || !data?.length) return;
+
+      const { data: publicUrl } = supabase.storage.from("jernih").getPublicUrl(data[0].path);
+      setUrl(publicUrl?.publicUrl || null);
+    };
+
+    fetchImage();
+  }, [path]);
+
+  return url;
+}
---
+++ hooks/use-require-auth.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { useAuth } from "@/components/context/auth-context";
+
+const MIN_LOADING_TIME = 400;
+
+export function useRequireAuth() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [ready, setReady] = useState(false);
+  const [timerDone, setTimerDone] = useState(false);
+
+  // tambahin routes lain
+  const publicRoutes = ["/", "/login", "/register", "/verification-sent", "/reset-password"];
+
+  useEffect(() => {
+    const t = setTimeout(() => setTimerDone(true), MIN_LOADING_TIME);
+    return () => clearTimeout(t);
+  }, []);
+
+  useEffect(() => {
+    if (!loading && timerDone) {
+      if (publicRoutes.includes(pathname) ) {
+        setReady(true);
+      } else {
+        if (!user) {
+          console.log('is this true')
+          router.push("/login");
+        } else {
+          setReady(true);
+        }
+      }
+    }
+  }, [loading, user, pathname, router, timerDone]);
+
+  return !ready;
+}
