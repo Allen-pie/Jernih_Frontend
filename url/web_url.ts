8204876a19adb2b088cac92dc70e564cbbbbd40e
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-01 13:11:32 +0700

    #feat web & api urls

--- url/web_url.ts
+++ url/web_url.ts
@@ -1,2 +1,15 @@
-const WEB_URL = 'http://localhost:3000';
-export default WEB_URL;
+// DEVELOPMENT
+const URLS = {
+    WEB : 'http://localhost:3000',
+    API : 'http://127.0.0.1:5000'
+} ;
+
+
+// PRODUCTION
+// const URLS = {
+//     WEB : 'http://localhost:3000',
+//     API : 'https://jernihbackend-production.up.railway.app'
+// } ;
+
+export default URLS;
+
