7d3fa1ae3e56460933668205b8e33bbc32a692d6
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-04 20:18:55 +0700

    #fix remove unecessary imports

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,14 +1,10 @@
 "use client";
-
 import Link from "next/link";
 import {
   LayoutDashboard,
   BarChart,
   FileText,
-  Users,
   Waves,
-  HelpCircle,
-  Compass,
   Newspaper,
 } from "lucide-react";
 import {
