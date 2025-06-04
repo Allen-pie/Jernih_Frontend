cc150778a92da77a62a7c39ea502cad5b5c0a488
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-05 03:20:47 +0700

    #fix url web prod

--- url/web_url.ts
+++ url/web_url.ts
@@ -1,15 +1,15 @@
 // DEVELOPMENT
-const URLS = {
-    WEB : 'http://localhost:3000',
-    API : 'http://127.0.0.1:5000'
-} ;
+// const URLS = {
+//     WEB : 'http://localhost:3000',
+//     API : 'http://127.0.0.1:5000'
+// } ;
 
 
 // PRODUCTION
-// const URLS = {
-//     WEB : 'https://jernih.vercel.app',
-//     API : 'https://jernihbackend-production.up.railway.app'
-// } ;
+const URLS = {
+    WEB : 'https://jernih.vercel.app',
+    API : 'https://jernihbackend-production.up.railway.app'
+} ;
 
 export default URLS;
 
