0f0b4ee9fc2e9cd3be042ed96544afbf4490a22e
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-05 03:18:44 +0700

    #fix dependect saveprofile

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -42,7 +42,7 @@ export default function AuthCallback() {
 
   useEffect(() => {
     if (session) saveProfile();
-  }, [session, saveProfile]);
+  }, [session]);
 
   return (
     <div className="flex items-center justify-center h-screen bg-white dark:bg-gray-900">
