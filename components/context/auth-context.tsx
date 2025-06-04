8096868b77376121f799097e3c6ac18379449b97
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-04 20:23:40 +0700

    #feat get profile data when login

--- components/context/auth-context.tsx
+++ components/context/auth-context.tsx
@@ -4,6 +4,14 @@ import { Session, User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { supabase } from '@/utils/supabase/client'
 
+interface Profile extends User {
+  role_id? : number;
+  age? : number;
+  phone_number? : string;
+  full_name ? : string;
+}
+
+
 interface AuthContextType {
   user: User | null;
   session: Session | null;
@@ -14,16 +22,34 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<Profile | null>(null);
   const [session, setSession] = useState<Session | null>(null);
   const [loading, isLoading] = useState<boolean>(true);
-
   const router = useRouter();
+
+
+  const getProfiles = async (user_id : string) => {
+    const {data : profile_data, error : profile_error} = await supabase.from('profiles').select().eq('id', user_id);
+    if (profile_data) return profile_data[0];
+    return null;
+  }
+
   useEffect(() => {
     const getSession = async () => {
-      const { data } = await supabase.auth.getSession();
+      const { data, error } = await supabase.auth.getSession();
+    
       if (data.session) {
-        setUser(data.session.user);
+        const profile = await getProfiles(data.session.user.id);
+        if (profile){  
+          setUser({
+            ...data.session.user,
+            ...profile
+          })
+        }
+        else {
+          setUser(data.session.user)
+        }
+
         setSession(data.session);
       } 
     
@@ -35,12 +61,23 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const { data: listener } = supabase.auth.onAuthStateChange( 
      async (_event, session) => {
         
-        if (_event == "PASSWORD_RECOVERY") {
-            console.log('tes');
-         }
+        // if (_event == "PASSWORD_RECOVERY") {
+        //     console.log('tes');
+        //  }
 
+         if (session){
+           const profile = await getProfiles(session.user.id);
+           if (profile){
+              setUser({
+                ...session?.user,
+                ...profile
+              })
+           }else{
+              setUser(session?.user);
+           }
+
+         }
         setSession(session);
-        setUser(session?.user || null);
       }
     );
 
