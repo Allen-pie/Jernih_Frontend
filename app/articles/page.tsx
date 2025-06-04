cbc9cafa901e7a5e3dcc4dabf0c0eb11016783cf
KevinLiong <kevin.liong@binus.ac.id>
2025-06-04 23:26:56 +0700

    # feat loading animation when fetching article

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+'use client'
+
+import React, { useEffect, useState } from "react";
 import { ArticleCard } from "@/components/article-card";
 // import { articles } from "@/utils/tempArticleData";
 import { fetchArticlesGuest } from "@/utils/supabase/article";
 import Footer  from "@/components/footer";
 import { ArticleGuest } from "../interfaces";
 
-export default async function ArticlesPage() {
-  const articles : ArticleGuest[] = await fetchArticlesGuest();
+export default function ArticlesPage() {
+  const [articles, setArticles] = useState<ArticleGuest[]>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  
+  useEffect(() => {
+    const fetchArticle = async() => {
+      const fetched = await fetchArticlesGuest();
+      setArticles(fetched as ArticleGuest[]);
+      setLoading(false);
+    }
+
+    fetchArticle(); 
+  }, []);
+
+  // const articles : ArticleGuest[] = await fetchArticlesGuest();
 
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-50 to-white">
@@ -27,20 +43,27 @@ export default async function ArticlesPage() {
             Artikel Terbaru
           </h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {articles.map((article, idx) => (
-            <ArticleCard
-              key={idx}
-              id={article.id}
-              title={article.title}
-              excerpt={article.excerpt}
-              image_url={article.image_url}
-              author={article.author}
-              publish_date={article.published_at}
-              comment_count={article.comment_count}
-            />
-          ))}
-        </div>
+        
+        {loading ? (
+          <div className="flex justify-center">
+            <div className="animate-spin h-6 w-6 rounded-full border-t-2 border-blue-500 border-solid" />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {articles?.map((article, idx) => (
+              <ArticleCard
+                key={idx}
+                id={article.id}
+                title={article.title}
+                excerpt={article.excerpt}
+                image_url={article.image_url}
+                author={article.author}
+                publish_date={article.published_at}
+                comment_count={article.comment_count}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <Footer />
     </div>
