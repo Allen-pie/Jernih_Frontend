a6bf173fd097cf297755f84642848381be5c19b2
Pieter Allen <pieter.allen@binus.ac.id>
2025-06-05 01:20:18 +0700

    #feat admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useAuth } from "@/components/context/auth-context";
+import { supabase } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function AdminLayout({children} : {children : React.ReactNode}){
+    const {session, user} = useAuth();
+    const router = useRouter();
+    const [ready, setReady] = useState<boolean>(false);
+     
+    useEffect(() => {
+        if (!session) return router.push('/login');
+
+        const getProfiles = async (user_id : string) => {
+            const {data : profile_data, error : profile_error} = await supabase.from('profiles').select().eq('id', user_id);
+            if (profile_data && profile_data[0].role_id == 1) {
+                setReady(true);
+            }
+            else return router.push("/auth/error");
+        }
+        
+        getProfiles(session.user.id);
+
+    },[router, session])
+
+    if (!ready){
+    return <div className="flex items-center justify-center h-screen bg-white dark:bg-gray-900">
+        <div className="animate-spin h-16 w-16 rounded-full border-t-4 border-blue-500 border-solid" />
+      </div>
+    }
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
